75045def5243e8a24b668f1e22da1c3867a6af85
jananasrr <jana2308303@miuegypt.edu.eg>
2025-06-15 03:41:07 +0300

    Enhance server configuration by using path module for static files and views, and set NODE_ENV to production in Vercel configuration.

--- index.js
+++ index.js
@@ -8,15 +8,21 @@ import usersRouter from "./routes/user.js";
 import frontRouter from "./routes/frontend.js";
 import chatbotRouter from "./routes/chatbot.js";
 import dashboardRouter from "./routes/dashboard.js";
-//salmaa
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use("/api/book", booksRouter);
 app.use("/api/order", ordersRouter);
